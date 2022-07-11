0156b49d09c2caf6dac9f1cdba83507fb25283fc
Melissa Longenberger <apple@Irish-iMac.hitronhub.home>
2022-07-11 19:46:18 -0400

    saving extra notes week 2

---
+++ lectures/week2/main.py
@@ -0,0 +1,20 @@
+pets = [
+        {'name':  'Copper', 'breed' : 'Beagle'},
+        {'name' : 'Bear', 'breed' : 'Shepard Mix'},
+        {'name' : 'Lucy', 'breed' : 'Chawinni'},
+        {'name' : 'Roxy', 'breed' : 'Red Nose Pit Shepard'}
+    ]
+
+def keyValue(list): # creating function passing in a list to be named during activation
+    for i in range(0, len(list)): # looping through the whole list
+        output = "" # creating var output setting to empty string so that it can hold info to follow
+        for key,val in list[i].items(): # for each key & value in the list of items do the following (items() are refering to the items inside the dict)
+            output += f" {key} - {val}," # change the value of output to equal the key - value
+        print("Printing output of each index:",output) # print output at end of each loop
+
+keyValue(pets) # activates function
+
+# Think of this way of doing a for loop like this in order to collect the eggs the farmer needs a basket.  Not just any basket but an empty one.  So line 10 is our way of creating not just a basket but an empty one.  Then on line 11 we need to tell the farmer what to put into this basket.  The Brown Hens aren't collected in the morning just the White ones for example.  So the instructions on line 11 are stating just what we want in the basket.  Line 12 is the act of putting those eggs into the basket and line 13 being outside of the loop will show the final results of what we told the farmer to do... collect the eggs
+
+
+# The output += means that we add the current value to what was already there... not overwrite it but add to it.  If we just put = then it would over write.  Since we want the collection of eggs to grow we have to add the + in there other wise it's like getting a new basket for each nest.
