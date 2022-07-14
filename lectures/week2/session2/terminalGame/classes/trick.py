e66dee115c62ca8f891ad2188aa7c65e595cbdec
melissa-24 <melissalongenberger24@gmail.com>
2022-07-14 14:25:07 -0400

    adjustments

--- lectures/week2/session2/terminalGame/classes/trick.py
+++ lectures/week2/session2/terminalGame/classes/trick.py
@@ -2,13 +2,6 @@ class Trick:
     def __init__(self,name, difficulty):
         self.name = name
         self.difficulty = difficulty
-        self.trickList = []
-
-    def tricks(self):
-        theTricks = []
-        for t in self.trickList:
-            theTricks.append(t)
-        print(theTricks)
 
 
     def leveledUp(self, level):
@@ -17,17 +10,3 @@ class Trick:
         print(f'Trick leveled up from {original} to {self.difficulty}')
         return self
 
-
-
-# Creating trick instances
-jump = Trick('Jumping Bricks', 2)
-sit = Trick('Sit when told', 2)
-speak = Trick('Bark on command', 1)
-dance = Trick('Dance when asked', 9)
-paw = Trick('Give 5 or shake hands', 5)
-
-jump.tricks()
-sit.tricks()
-speak.tricks()
-dance.tricks()
-paw.tricks()
